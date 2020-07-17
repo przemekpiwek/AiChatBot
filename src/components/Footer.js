f31d50f181c87abe0e1fb61ee834985ac3c80491
Przemek Piwek <przemekpiwek@gmail.com>
2020-07-17 01:02:30 -0600

    Input clears after submit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -56,6 +56,7 @@ function Footer({ messages, setMessages }) {
               let newMessageArray = [...messages];
               newMessageArray.push(json);
               setMessages(newMessageArray);
+              setValue("");
             }
           }}
         />
