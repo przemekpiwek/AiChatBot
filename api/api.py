754fab9cbd50db19e438ed738c3c637313406fd3
Przemek Piwek <przemekpiwek@gmail.com>
2020-07-22 13:39:49 -0600

    -test

--- api/api.py
+++ api/api.py
@@ -143,6 +143,7 @@ def api():
     return response
         
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 print("mounted server")
